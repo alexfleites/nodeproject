1fed66e255aab89bf0b120cde59513aa6c0e5cfa
Alexander Fleites <alex240505@gmail.com>
2017-10-31 17:33:18 +0000

    Commit ejs created

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,26 @@
-var aloha = 'Aloha World!!!';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('bower_components'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+   //res.send('Aloha World!');
+   res.render('index', {
+       list: ['first val', '2nd val', '3rd val'],
+       nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact'] 
+   });
+});
+
+app.get('/routing', function(req, res){
+   res.send('Aloha Routing!'); 
+});
+
+app.listen(port, function(err){
+   console.log('The server is running on port: ' + port); 
+});
